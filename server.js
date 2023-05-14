30919e509d2cda52ef588a7bd99bb9b088fdca0a
lazmil <lazmil1999@gmail.com>
2023-05-15 00:09:48 +0400

    Update port configuration01

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const openai = new OpenAIApi(
   new Configuration({
