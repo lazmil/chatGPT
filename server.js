b2a63c27c1a77fe043e47837778c1a139cf8b93d
lazmil <lazmil1999@gmail.com>
2023-05-21 15:28:10 +0400

    done

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3001;
 
 const openai = new OpenAIApi(
   new Configuration({
-    apiKey: process.env.OPENAI_API_KEY, // Use API key from environment variable
+    apiKey: process.env.VERCEL_API_KEY, // Use API key from environment variable
   })
 );
 
